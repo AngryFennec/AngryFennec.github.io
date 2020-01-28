75f11fd9455786707ed2a00df9dffaddfd204a02
AngryFennec <shellfix@gmail.com>
2020-01-28 17:20:11 +0300

    nothman

---
+++ northman/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * подключение js-модулей
+ * //= modules/file.js
+ */
+
+
+var mySwiper = new Swiper('.swiper-container', {
+
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+speed: 1
+});
+
+/*
+mySwiper.on('slideChangeTransitionStart', function () {
+  var prev = mySwiper.previousIndex-1;
+  var next = mySwiper.activeIndex;
+  var prevSlide = mySwiper.slides[prev];
+  var nextSlide = mySwiper.slides[next];
+  prevSlide.querySelector('img').classList.remove('animated');
+  nextSlide.querySelector('img').classList.add('animated');
+  console.log(nextSlide);
+});
+*/
