eee9ab134fc1d56137c2a3298f4b389d0a0b2e61
AngryFennec <angryfennecus@gmail.com>
2020-02-14 21:55:18 +0300

    Слайдер

--- pferd/js/main.js
+++ pferd/js/main.js
@@ -31,13 +31,34 @@ if(input2) im2.mask(input2);
 (function() {
   var headerSelector = document.querySelector('.main__header-swiper .swiper-container');
   if (headerSelector) {
-  var swiper = new Swiper(headerSelector, {
+  var headerSwiper = new Swiper(headerSelector, {
       direction: 'vertical',
       slidesPerView: 1,
       pagination: {
         el: '.main__header-pagination',
         clickable: true,
       },
+    });
+  }
+
+  var industrySelector = document.querySelector('.industry .swiper-container');
+  if (industrySelector) {
+  var industrySwiper = new Swiper(industrySelector, {
+      slidesPerView: 1,
+      pagination: {
+        el: '.industry__pagination',
+        clickable: true,
+      },
+      loop: true,
+      navigation: {
+        nextEl: '.industry__next',
+        prevEl: '.industry__prev',
+      },
+        breakpoints: {
+      1919: {
+      slidesPerView: 2
+    },
+  }
     });
   }
 })();
