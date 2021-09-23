73c5ea65e73810e35d9427adf0c0ab7991e92808
Angry Fennec <shellfix@gmail.com>
2021-09-23 09:56:52 +0300

    Текстовая

---
+++ karta-blue/js/main.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line
+var galleryThumbs = new Swiper('.article__gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var galleryText = new Swiper('.article__gallery-text', {
+  slidesPerView: 1,
+  noSwiping: true,
+  allowTouchMove: false,
+});
+
+var galleryTop = new Swiper('.article__gallery-top', {
+  freeMode: true,
+  breakpoints: {
+    // when window width is >= 1024
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      thumbs: {
+        swiper: galleryThumbs,
+      }
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+galleryTop.on('slideChange', function () {
+  galleryText.slideTo(galleryTop.activeIndex);
+});
+// eslint-disable-next-line
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+const close = document.querySelector('.header__close');
+
+
+burger.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  menu.classList.add('header__menu_active');
+  close.classList.add('header__close_active');
+});
+
+close.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  menu.classList.remove('header__menu_active');
+  close.classList.remove('header__close_active');
+});
