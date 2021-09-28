f2b7185bbfb343bac9fd85ac67142c176b8f3254
Angry Fennec <shellfix@gmail.com>
2021-09-28 08:57:33 +0300

    blue

--- karta-blue/js/main.js
+++ karta-blue/js/main.js
@@ -82,3 +82,105 @@ popupSpans.forEach((item) => {
 
   });
 });
+// eslint-disable-next-line
+'use strict';
+
+let buttonsSwiper = Swiper;
+let cardsSwiper = Swiper;
+
+let init = false;
+let mobile = window.matchMedia('(min-width: 0px) and (max-width: 1023px)');
+let desktop = window.matchMedia('(min-width: 1024px)');
+let flag = false;
+
+function buttonsSwiperMode() {
+
+  // Enable (for mobile)
+  if(mobile.matches) {
+    if (!init) {
+      init = true;
+      buttonsSwiper = new Swiper('.find-tags__buttons-list', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        freeMode: true,
+      });
+      cardsSwiper = new Swiper('.find-cards__cards-list', {
+        slidesPerView: 'auto',
+        spaceBetween: 8,
+        freeMode: true,
+      });
+    }
+  }
+
+  // Disable (for desktop)
+  else if (desktop.matches) {
+    buttonsSwiper.destroy(true, false);
+    cardsSwiper.destroy(true, false);
+
+    init = false;
+  }
+}
+
+
+
+const partnersBtns = Array.from(document.querySelectorAll('.find-tags__button-item'));
+partnersBtns.forEach(item => {
+  item.addEventListener('click', function(e) {
+    item.classList.contains('find-tags__button-item--active') ? item.classList.remove('find-tags__button-item--active') : item.classList.add('find-tags__button-item--active');
+  })
+});
+
+
+
+
+/* On Load
+**************************************************************/
+window.addEventListener('load', function() {
+  buttonsSwiperMode();
+  flag = mobile.matches;
+});
+
+/* On Resize
+**************************************************************/
+window.addEventListener('resize', function() {
+  buttonsSwiperMode();
+});
+// eslint-disable-next-line
+var swiper = new Swiper('.photos__list', {
+  spaceBetween: 0,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+});
+// eslint-disable-next-line
+$('.sliding-link').click(function (e) {
+  e.preventDefault();
+  var aid = $(this).attr('href');
+  $('html,body').animate({scrollTop: $(aid).offset().top}, 'slow');
+});
+// eslint-disable-next-line
+const itemsSwiper = new Swiper('.items__container', {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+const wordsArray = ['слово-1', 'слово-2', 'слово-3', 'слово-4'];
+const wordElement = document.querySelector('.promo__title span');
+
+let index = 0;
+
+setInterval(function() {
+  if (index >= wordsArray.length) {
+    index = 0;
+  }
+  wordElement.textContent = wordsArray[index];
+  index++;
+}, 2000);
---
+++ karta-blue/js/modules/photos.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.photos__list', {
+  spaceBetween: 0,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+});
